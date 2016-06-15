d8d2f57fe882d47b612062cd3b2c517c91a676fc
Risto Yrjänä <risto@vaadin.com>
2016-06-15 09:41:23 +0300

    Add example addon

--- src/main/java/com/example/MyUI.java
+++ src/main/java/com/example/MyUI.java
@@ -1,16 +1,26 @@
 package com.example;
 
+import org.vaadin.teemu.switchui.Switch;
+
+import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 @SpringUI
 @Widgetset("com.example.Widgetset")
+@Theme(ValoTheme.THEME_NAME)
 public class MyUI extends UI{
+
     @Override
     protected void init(VaadinRequest request) {
-        setContent(new Label("Hello!"));
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setSizeFull();
+        layout.setMargin(true);
+        layout.addComponent(new Switch());
+        setContent(layout);
     }
 }
